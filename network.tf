63474e128013460a8a9b798ab2c6509fb483c882
Ni1ghtmarewow <114662065+Ni1ghtmarewow@users.noreply.github.com>
2022-12-06 14:41:34 +0100

    Add files via upload

--- network.tf
+++ network.tf
@@ -1,9 +1,16 @@
 provider "aws" {
+  alias = "main"
+  profile = "UniUser"
+  region = "eu-central-1"
+}
+
+provider "aws" {
   region = "${var.aws_region}"
 }
 
 # Creating VPC
 resource "aws_vpc" "demovpc" {
+  provider = aws.main
   cidr_block       = "${var.vpc_cidr}"
   instance_tenancy = "default"
 
@@ -14,11 +21,13 @@ resource "aws_vpc" "demovpc" {
 
 # Creating Internet Gateway 
 resource "aws_internet_gateway" "demogateway" {
+  provider = aws.main
   vpc_id = "${aws_vpc.demovpc.id}"
 }
 
 # Grant the internet access to VPC by updating its main route table
 resource "aws_route" "internet_access" {
+  provider = aws.main
   route_table_id         = "${aws_vpc.demovpc.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.demogateway.id}"
@@ -26,6 +35,7 @@ resource "aws_route" "internet_access" {
 
 # Creating 1st subnet 
 resource "aws_subnet" "cicdsubnet" {
+  provider = aws.main
   vpc_id                  = "${aws_vpc.demovpc.id}"
   cidr_block             = "${var.subnet_cidr}"
   map_public_ip_on_launch = true
@@ -38,6 +48,7 @@ resource "aws_subnet" "cicdsubnet" {
 
 # Creating 2nd subnet 
 resource "aws_subnet" "cicdsubnet2" {
+  provider = aws.main
   vpc_id                  = "${aws_vpc.demovpc.id}"
   cidr_block             = "${var.subnet1_cidr}"
   map_public_ip_on_launch = true
